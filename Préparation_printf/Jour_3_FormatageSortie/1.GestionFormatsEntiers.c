cbafdda5ee6f80d247d22dfad791794157e5ff2d
maatthhiieeuu <bulle8867@gmail.com>
2023-12-20 08:03:32 +0100

    mise à jour du travail effectué à l'école .

--- Préparation_printf/Jour_3_FormatageSortie/1.GestionFormatsEntiers.c
+++ Préparation_printf/Jour_3_FormatageSortie/1.GestionFormatsEntiers.c
@@ -1,24 +1,14 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-
-size_t	ft_intlen(const int *str)
-{
-	size_t	i;
-
-	i = 0;
-	if (!str)
-		return(-1);
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 void	ft_putchar(int c)
 {
 	write(1, &c, 1);
 }
+
+/*-----------------------------------------------------------------------------*/
+
 
 void	ft_putnbr_fd(int n, int fd)
 {
@@ -40,7 +30,10 @@ void	ft_putnbr_fd(int n, int fd)
 	write(fd, &c, 1);
 }
 
-void 	decimalToOctal(long int num)
+/*-----------------------------------------------------------------------------*/
+
+
+void 	decimalToOctal(int num)
 {
 	int i;
 	int octalNum[50];
@@ -59,14 +52,110 @@ void 	decimalToOctal(long int num)
 		ft_putnbr_fd(octalNum[i], 1);
 		i--;
 	}
-	ft_putchar('\n');
+}
+
+/*-----------------------------------------------------------------------------*/
+
+
+void	decimalToHexadecimal(int num)
+{
+	int	i;
+	int	hexaNum[50];
+	char hexadecimalMin[] = "0123456789abcdef";
+
+	i = 0;
+	while (num != 0)
+	{
+		hexaNum[i] = num % 16;
+		num /= 16;
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		if (hexaNum[i] >= 0 && hexaNum[i] <= 16)
+		{
+			char temp;
+
+			temp = hexadecimalMin[hexaNum[i]];
+			ft_putchar(temp);
+		}
+		i--;
+	}
+}
+
+/*-----------------------------------------------------------------------------*/
+
+
+void	format(char *s, ...)
+{
+	int	i;
+
+	i = 0;
+	va_list args;
+
+	va_start(args, s);
+	if (!s)
+		return;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '%')
+		{
+
+			if (s[i + 1] == 'd')
+			{
+				int tempNumberDec;
+
+				tempNumberDec = va_arg(args, int);
+				ft_putnbr_fd(tempNumberDec, 1);
+				ft_putchar(' ');
+				i++;
+			}	
+			else if (s[i + 1] == 'o')
+			{
+				int tempNumberOct;
+
+				tempNumberOct = va_arg(args, int);
+				decimalToOctal(tempNumberOct);
+				ft_putchar(' ');
+				i++;
+			}
+			else if (s[i + 1] == 'x')
+			{
+				int tempNumberHex;
+
+				tempNumberHex = va_arg(args, int);
+				decimalToHexadecimal(tempNumberHex);
+				ft_putchar(' ');
+				i++;
+			}
+			else if (s[i + 1] == '%')
+			{
+				ft_putchar('%');
+				ft_putchar(' ');
+			}
+			i++;
+		}
+		else
+		ft_putchar(s[i]);
+	i++;
+	}
+	va_end(args);
 
 }
 
+/*-----------------------------------------------------------------------------*/
+
+
 int main(void)
 {
-	long int nbr;
+	int num;
 
-	nbr = 24;  //en octal = 6537
-	decimalToOctal(nbr);
+	num = 195;
+	format("en decimal %d s'ecrit: %d.", num, num);
+	ft_putchar('\n');
+	format("en octal %d s'ecrit: %o.", num, num);
+	ft_putchar('\n');
+	format("en hexadecimal %d s'ecrit: %x.", num, num);
+	ft_putchar('\n');
 }
