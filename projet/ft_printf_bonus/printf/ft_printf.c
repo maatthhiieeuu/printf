4d5ee2bcc98f289057a2b7503ddd123bfe1e7226
maatthhiieeuu <148256781+maatthhiieeuu@users.noreply.github.com>
2024-01-16 11:53:16 +0100

    Add files via upload

--- projet/ft_printf_bonus/printf/ft_printf.c
+++ projet/ft_printf_bonus/printf/ft_printf.c
@@ -1,84 +1,63 @@
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mboegler <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 11:24:12 by mboegler          #+#    #+#             */
+/*   Updated: 2024/01/16 11:24:18 by mboegler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "ft_printf.h"
 
-static	void	dispatch_and_route_to_bonus(const char *format, va_list args, int *i);
+static void	dispatch(const char *format, va_list args, int *i);
 
-/*----------------------------------------------------------------------------*/
-
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-	//printf("\nDébut de fonction de ft_printf\n");
 	int	i;
+	va_list args;
 
 	i = 0;
-	va_list args;
 	va_start(args, format);
 	while(format[i])
 	{
 		if ((format[i] == '%') || (format[i] == 92))
-		{
-			dispatch_and_route_to_bonus(format, args, &i);
-		}
+			dispatch(format, args, &i);
 		else
 			ft_putchar_fd(format[i], 1);
 		i++;
 	}
 	va_end(args);
-	
 	return (i);
 }
 
-/*----------------------------------------------------------------------------*/
-
-static	void	dispatch_and_route_to_bonus(const char *format, va_list args, int *i) 
+static	void	dispatch(const char *format, va_list args, int *i) 
 {
-	//printf("\nDébut de dispatch_and_route_to_bonus :\n");
 	if (!format)
 		return;
-	if (format[*i + 1] == 'c'){
-		//printf("	cCc\n");
+	if (format[*i + 1] == 'c')
 		print_character(format, args, &i);
-	}
-	else if (format[*i + 1] == 's'){
-		//printf("	sSs\n");
+	else if (format[*i + 1] == 's')
 		print_string(format, args, &i);
-	}
-	else if (format[*i + 1] == 'p'){
-		//printf("	pPp\n");
+	else if (format[*i + 1] == 'p')
 		print_pointer_address(format, args, &i);
-	}
-	else if (format[*i + 1] == 'd'){
-		//printf("	dDd\n");
+	else if (format[*i + 1] == 'd')
 		print_signed_int(format, args, &i);
-	}
-	else if (format[*i + 1] == 'i'){
-		//printf("	iIi\n");
+	else if (format[*i + 1] == 'i')
 		print_integer(format, args, &i);
-	}
-	else if (format[*i + 1] == 'u'){
-		//printf("	uUu\n");
+	else if (format[*i + 1] == 'u')
 		print_unsigned_int(format, args, &i);
-	}
-	else if (format[*i + 1] == 'x'){
-		//printf("	xXx\n");
+	else if (format[*i + 1] == 'x')
 		print_hex_lowercase(format, args, &i);
-	}
-	else if (format[*i + 1] == 'X'){
-		//printf("	XXX\n");
+	else if (format[*i + 1] == 'X')
 		print_hex_uppercase(format, args, &i);
-	}
-	else if (format[*i + 1] == '%'){
-		//printf("	%%%%%%\n");
+	else if (format[*i + 1] == '%')
 		print_percent_sign(format, &i);
-	}
 	#ifdef BONUS
-	else{
-		//printf("	111\n");
+	else
 		coordination_initialisation_structure(format, args, i);
-	}
 	#endif
 }
-
